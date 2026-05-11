3d75dfe5d6dc79f2e8781021854198ab7d3ce545
Taha Nachit <tnnachit@gmail.com>
2026-05-11 08:49:01 +0100

    Debut Controle

---
+++ backend/assurance-app/src/main/java/ma/nachit/taha/assurance_app/AssuranceAppApplication.java
@@ -0,0 +1,13 @@
+package ma.nachit.taha.assurance_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssuranceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssuranceAppApplication.class, args);
+	}
+
+}
